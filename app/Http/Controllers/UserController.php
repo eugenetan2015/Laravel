0c78ed511e15f702c392bde150afe8d2816e59da
eugenetan2015 <eugene.tan.2015@sis.smu.edu.sg>
2017-11-24 17:00:38 +0800

    Login and signup routes
    Changelog:
    1. Added user auth (signup and signin routes)
    2. Added logic to UserController for routing
    3. Lesson 4 of laravel tutorial

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,14 @@
 namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller{
+
+    public function getDashboard(){
+        return view('dashboard');
+    }
+
     public function postSignUp(Request $request){
         $email = $request['email'];
         $first_name = $request['first_name'];
@@ -15,10 +21,16 @@ class UserController extends Controller{
         $user->password = $password;
 
         $user->save();
-
-        return redirect()->back();
+        Auth::login($user);
+        return redirect()->route('dashboard');
     }
+
     public function postSignIn(Request $request){
 
+        if(Auth::attempt(['email' => $request['email'], 'password' => $request['password']])){ // take array of arguments to authenticate user
+            return redirect()->route('dashboard');
+        }else{
+            return redirect()->back();
+        }
     }
 }
--- app/User.php
+++ app/User.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Model implements Authenticatable
 {
-    //
+    use \Illuminate\Auth\Authenticatable;
+    //to authenticate users
+    //implement the contract will implement authenticable trait
+    //authenticate users via this contract
 }
